984e6e3d4fb17a385ff783af7322d50bbdc742d2
Ic3Tank <61137113+IceTank@users.noreply.github.com>
2025-11-10 23:57:47 +0100

    Add instance reference to label

--- src/main/java/org/icetank/module/MetricsModule.java
+++ src/main/java/org/icetank/module/MetricsModule.java
@@ -3,15 +3,18 @@ package org.icetank.module;
 import com.zenith.module.api.Module;
 import io.prometheus.metrics.exporter.httpserver.HTTPServer;
 import io.prometheus.metrics.instrumentation.jvm.JvmMetrics;
-import org.icetank.MetricsPlugin;
 import org.icetank.api.ServiceAnnouncer;
 import org.icetank.metric.Metrics;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.concurrent.*;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 import static org.icetank.MetricsPlugin.LOG;
+import static org.icetank.MetricsPlugin.PLUGIN_CONFIG;
 
 public class MetricsModule extends Module {
     HTTPServer server = null;
@@ -21,7 +24,7 @@ public class MetricsModule extends Module {
 
     @Override
     public boolean enabledSetting() {
-        return MetricsPlugin.PLUGIN_CONFIG.enabled;
+        return PLUGIN_CONFIG.enabled;
     }
 
     @Override
@@ -38,10 +41,11 @@ public class MetricsModule extends Module {
                     .port(0)
                     .buildAndStart();
 
-            String accountName = MetricsPlugin.PLUGIN_CONFIG.serviceDiscovery.accountName;
+            String accountName = PLUGIN_CONFIG.serviceDiscovery.accountName;
             String serviceName = "zenith-proxy-" + accountName;
-            Map<String, String> labels = new HashMap<>(MetricsPlugin.PLUGIN_CONFIG.serviceDiscovery.labels);
-            labels.put("accountName", accountName);
+            Map<String, String> labels = new HashMap<>(PLUGIN_CONFIG.serviceDiscovery.labels);
+            labels.put("accountName", accountName); // For grafana queries
+            labels.put("instance", PLUGIN_CONFIG.serviceDiscovery.targetHost); // Prevent cardinality explosion
 
             if (accountName.isEmpty()) {
                 LOG.error("Service ID is empty, configure with zenith instance name");
@@ -51,13 +55,13 @@ public class MetricsModule extends Module {
                 LOG.error("Metrics server failed to start, cannot announce serviceName");
                 throw new RuntimeException("Metrics server failed to start");
             }
-            String target = MetricsPlugin.PLUGIN_CONFIG.serviceDiscovery.targetHost + ":" + server.getPort();
+            String target = PLUGIN_CONFIG.serviceDiscovery.targetHost + ":" + server.getPort();
 
             ServiceAnnouncer.ServiceInfo info = new ServiceAnnouncer.ServiceInfo(accountName, serviceName, target, labels, 60);
             CompletableFuture.runAsync(() -> {
                 try {
-                    announcer.registerService(MetricsPlugin.PLUGIN_CONFIG.serviceDiscovery.host,
-                            MetricsPlugin.PLUGIN_CONFIG.serviceDiscovery.port, info);
+                    announcer.registerService(PLUGIN_CONFIG.serviceDiscovery.host,
+                            PLUGIN_CONFIG.serviceDiscovery.port, info);
                 } catch (Exception ex) {
                     LOG.error("Failed to register serviceName. Disabling.", ex);
                     disable();
@@ -68,8 +72,8 @@ public class MetricsModule extends Module {
                 }
                 scheduler.scheduleAtFixedRate(() -> {
                     try {
-                        announcer.sendHeartbeat(MetricsPlugin.PLUGIN_CONFIG.serviceDiscovery.host,
-                                MetricsPlugin.PLUGIN_CONFIG.serviceDiscovery.port, info);
+                        announcer.sendHeartbeat(PLUGIN_CONFIG.serviceDiscovery.host,
+                                PLUGIN_CONFIG.serviceDiscovery.port, info);
                     } catch (Exception ex) {
                         LOG.error("Failed to send heartbeat for serviceName {}: {}", accountName, ex.getMessage());
                     }
