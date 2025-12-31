5587e6114d0ee9ce58c4825ae6efa6048083ebcb
Ic3Tank <61137113+IceTank@users.noreply.github.com>
2025-12-31 01:41:13 +0100

    Separate entities into context queue and context game

--- src/main/java/org/icetank/metric/metrics/EntitiesInfo.java
+++ src/main/java/org/icetank/metric/metrics/EntitiesInfo.java
@@ -1,6 +1,7 @@
 package org.icetank.metric.metrics;
 
 
+import com.zenith.Proxy;
 import io.prometheus.metrics.core.metrics.Counter;
 import io.prometheus.metrics.core.metrics.GaugeWithCallback;
 import io.prometheus.metrics.model.registry.PrometheusRegistry;
@@ -23,21 +24,25 @@ import static java.util.stream.Collectors.reducing;
 public class EntitiesInfo implements Registerable {
     private static Counter entityCounter;
     private static final List<Integer> uniqueEntityIds = new ArrayList<>();
-    private static int highestEntityId = 0;
+    private static int highestEntityIdGame = 0;
+    private static int highestEntityIdQueue = 0;
+    private static String CONTEXT_GAME = "game";
+    private static String CONTEXT_QUEUE = "queue";
     @Override
     public void register(PrometheusRegistry registry) {
         GaugeWithCallback.builder()
                 .name("zenith_entities_current")
                 .help("Number of items dropped in the world")
-                .labelNames("type")
+                .labelNames("type", "context")
                 .callback(callback -> {
+                    String context = Proxy.getInstance().isInQueue() ? CONTEXT_QUEUE : CONTEXT_GAME;
                     Map<String, Integer> entityCountMap = CACHE.getEntityCache().getEntities().values().stream()
                             .collect(groupingBy(e -> {
                                 var entityData = ENTITY_DATA.getEntityData(e.getEntityType());
                                 return entityData != null ? entityData.name() : "unknown";
                             }, reducing(0, e -> 1, Integer::sum)));
                     for (Map.Entry<String, Integer> entry : entityCountMap.entrySet()) {
-                        callback.call(entry.getValue(), entry.getKey());
+                        callback.call(entry.getValue(), entry.getKey(), context);
                     }
                 })
                 .register(registry);
@@ -49,7 +54,11 @@ public class EntitiesInfo implements Registerable {
         GaugeWithCallback.builder()
                 .name("zenith_entities_highest_id")
                 .help("Highest entity ID encountered")
-                .callback(callback -> callback.call(highestEntityId))
+                .callback(callback -> {
+                    callback.call(highestEntityIdGame, CONTEXT_GAME);
+                    callback.call(highestEntityIdQueue, CONTEXT_QUEUE);
+                })
+                .labelNames("context")
                 .register(registry);
     }
 
@@ -69,12 +78,22 @@ public class EntitiesInfo implements Registerable {
     }
 
     public static void recordHighestEntityId(int entityId) {
-        if (entityId > highestEntityId) {
-            highestEntityId = entityId;
-            return;
-        }
-        if (entityId < 0 && highestEntityId > 0) {
-            highestEntityId = entityId;
+        if (Proxy.getInstance().isInQueue()) {
+            if (entityId > highestEntityIdQueue) {
+                highestEntityIdQueue = entityId;
+                return;
+            }
+            if (entityId < 0 && highestEntityIdQueue > 0) {
+                highestEntityIdQueue = entityId;
+            }
+        } else {
+            if (entityId > highestEntityIdGame) {
+                highestEntityIdGame = entityId;
+                return;
+            }
+            if (entityId < 0 && highestEntityIdGame > 0) {
+                highestEntityIdGame = entityId;
+            }
         }
     }
 }
